208eee7dc922d7dab95c52237f9a7b39753d1665
habelamb <habelamb@gmail.com>
2026-09-15 11:50:13 +0900

    未使用のuseRefを削除

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 
 import type { Body } from "../types/ResponseJson";
 import Description from "../components/Description";
