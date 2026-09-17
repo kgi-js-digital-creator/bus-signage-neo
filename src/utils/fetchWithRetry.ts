7253ba0f28b76dbc52acef8ebb0c11802a83c98d
cadestoria <243375619+cadestoria@users.noreply.github.com>
2026-09-17 09:54:06 +0900

    サーバーエラー時の処理を追加

--- src/utils/fetchWithRetry.ts
+++ src/utils/fetchWithRetry.ts
@@ -1,7 +1,7 @@
 export default async function fetchWithRetry(url: string, options: RequestInit = {}, retry: number = 3, delay: number = 1000): Promise<Response> {
   try {
     const response = await fetch(url, options);
-    if (response.status === 404 && retry > 0) {
+    if ((response.status === 404 || response.status >= 500) && retry > 0) {
       await new Promise((resolve) => setTimeout(resolve, delay));
       return fetchWithRetry(url, options, retry - 1, delay * 2);
     }
